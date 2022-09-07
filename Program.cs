2830bdaadba70517f2515bf6d33f360c5c045ada
RThompson153 <robbyt15@gmail.com>
2022-09-07 18:53:42 -0500

    working

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using DevShed;
+using DevShed.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IStateContainer, StateContainer>();
 
 await builder.Build().RunAsync();
