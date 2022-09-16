7d052f431a5fb9d9f752aaa145a5bd9f77ed2ef3
Robert Thompson <Robert.Thompson@clickbond.com>
2022-09-16 14:17:59 -0500

    Working

--- Services/StateContainer.cs
+++ Services/StateContainer.cs
@@ -1,12 +1,21 @@
 ﻿using DSComponents;
+using DSComponents.Input.Models;
 using System;
+using System.Collections.Generic;
 
 namespace DevShed.Services
 {
     public class StateContainer : IStateContainer
     {
         public IDSCService DSCService { get; set; }
-        
+
+        private IEnumerable<DSCInputConfig> _dscInputConfigs;
+        public IEnumerable<DSCInputConfig> DSCInputConfigs
+        {
+            get => _dscInputConfigs ?? new List<DSCInputConfig>();
+            set => _dscInputConfigs = value;
+        }
+
         public StateContainer(IDSCService dscService)
         {
             DSCService = dscService;
