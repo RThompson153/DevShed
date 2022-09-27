0cfebac3a86719a175e1d45aa1632d3a5dab1cd8
RThompson153 <robbyt15@gmail.com>
2022-09-26 20:17:44 -0500

    Working

---
+++ Models/TableRow.cs
@@ -0,0 +1,11 @@
+﻿namespace DevShed.Models
+{
+	public class TableRow
+	{
+		public string Property { get; set; }
+		public string Type { get; set; }
+		public string DefaultValue { get; set; }
+		public string Binding { get; set; }
+		public string Description { get; set; }
+	}
+}
