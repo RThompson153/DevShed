4eee897bb604698074b6903ddea11ed7f5c52743
RThompson153 <robbyt15@gmail.com>
2022-11-21 19:36:58 -0600

    Working

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@ using System.Net.Http;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IDSCService, DSCService>();
 builder.Services.AddScoped<IStateContainer, StateContainer>();
